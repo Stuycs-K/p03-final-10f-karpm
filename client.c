1eb64cd5fdd732b5730fe4f8886048197d2ad3b3
Matvei Karp <matthewkarpstuy@gmail.com>
2026-01-08 15:24:37 -0500

    regrettably dont have the lab 16 final code on the lab computers, so ill need to redo it (should help remember how it works though)

--- client.c
+++ client.c
@@ -1,3 +1,27 @@
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <netdb.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "networking.h"
+
+void clientLogic(int server_socket){
+
+}
+
+int main(int argc, char *argv[] ) {
+  char* IP = "127.0.0.1";
+  if(argc>1){
+    IP=argv[1];
+  }
+  int server_socket = client_tcp_handshake(IP);
+  printf("client connected.\n");
+  clientLogic(server_socket)
+}
+
---
+++ mainServer.c
@@ -0,0 +1,11 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+
