cd82cf220332d6d5d5110bbb650ceba72b643b10
Matvei Karp <matthewkarpstuy@gmail.com>
2026-01-08 15:35:03 -0500

    mend

--- mainServer.c
+++ mainServer.c
@@ -8,4 +8,17 @@
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "networking.h"
+
+void subserver_logic(int client_socket){
+
+}
+
+int main(int argc, char *argv[] ) { 
+  int listen_socket = server_setup(); 
+
+
+  int client_socket = server_tcp_handshake(listen_socket);
+
+}
 
