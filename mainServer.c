9412c51797a163e085786036acb5c557ec6423a3
Matvei Karp <matthewkarpstuy@gmail.com>
2026-01-16 06:04:41 -0500

    first implementation of selector, next step is to make it send stuff to other clients, not to the same one

--- mainServer.c
+++ mainServer.c
@@ -21,7 +21,7 @@ while (1){
 outcount = recv(client_socket, buf, 255, 0);
 if(!strcmp("exit", buf)){
 printf("exiting");
-break;
+exit(0);
 }
 if(outcount){
   printf("%s\n", buf);
@@ -31,14 +31,30 @@ if(outcount){
 }
 
 int main(int argc, char *argv[] ) {
-//  while (1){
   int listen_socket = server_setup();
+  fd_set read_fds;
+  FD_ZERO(&read_fds);
+  FD_SET(listen_socket, &read_fds);
+  int max_descriptor = listen_socket;
 
-
+while(1){
+  fd_set temp_fds;
+  FD_ZERO(&temp_fds);
+  temp_fds = read_fds;
+  int state = select(max_descriptor+1, &temp_fds, NULL, NULL, NULL);
+  if (FD_ISSET(listen_socket, &temp_fds)){
   int client_socket = server_tcp_handshake(listen_socket);
-  if(fork()==0){
-  subserver_logic(client_socket);
+  if (client_socket>max_descriptor){
+    max_descriptor = client_socket;
   }
+  FD_SET(client_socket, &read_fds);
+}else{
+  for (int i = 0; i<= max_descriptor; i++){
+    if (FD_ISSET(i, &temp_fds)){
+      subserver_logic(i);
+    }
+  }
+}
+}
 
-//}
 }
